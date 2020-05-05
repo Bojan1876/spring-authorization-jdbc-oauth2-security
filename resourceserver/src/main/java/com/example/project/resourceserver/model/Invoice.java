0b2a566d6a4f912fffa199c239f50d046ac5c091
Bojan Martinovic <bojanmartinovic@ymail.com>
2020-05-05 20:40:04 +0200

    Invoice CRUD jdbc commit

--- resourceserver/src/main/java/com/example/project/resourceserver/model/Invoice.java
+++ resourceserver/src/main/java/com/example/project/resourceserver/model/Invoice.java
@@ -11,13 +11,10 @@ public class Invoice {
 
 	@Id
 	private Long id;
-	//oovo mogu menjati kroz approve
 	private String invoiceNumber;
 	private String dateOfInvoice;
 	private String description;
 	private String amount;
-	
-	//kada je approved ne mogu menjati status, niti updatovati, niti brisati
 	private InvoiceStatus status;
 
 }
